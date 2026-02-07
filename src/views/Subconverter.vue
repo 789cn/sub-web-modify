d5c758f7d3c0be7e96fca7807beb35541fd7a11e
789cn <34140884+789cn@users.noreply.github.com>
2026-02-08 00:01:58 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -26,9 +26,9 @@
                   <el-option v-for="(v, k) in options.clientTypes" :key="k" :label="k" :value="v"></el-option>
                 </el-select>
               </el-form-item>
-              <el-form-item label="后端地址:">
+              <el-form-item label="https://sub.789cn.cn:">
                 <el-select v-model="form.customBackend" allow-create filterable @change="selectChanged"
-                  placeholder="可输入自己的后端" style="width: 100%">
+                  placeholder="https://sub.789cn.cn" style="width: 100%">
                   <el-option v-for="(v, k) in options.customBackend" :key="k" :label="k" :value="v"></el-option>
                 </el-select>
               </el-form-item>
@@ -1304,3 +1304,4 @@ export default {
   }
 };
 </script>
+
