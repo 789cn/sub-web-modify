3e4189a0cf14eb6a08aaee4f1d2216b2df68a9ce
789cn <34140884+789cn@users.noreply.github.com>
2026-02-08 00:58:25 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -421,7 +421,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://url.v1.mk" : this.getUrlParam(),
+        customBackend: this.getUrlParam() == "" ? "https://sub.789cn.cn" : this.getUrlParam(),
         shortType: "https://v1.mk/short",
         remoteConfig: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini",
         excludeRemarks: "",
@@ -945,6 +945,7 @@ export default {
   }
 };
 </script>
+
 
 
 
